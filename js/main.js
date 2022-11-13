e2fddc893f626e99856a5a7b4c68b15807e45caf
asari20 <ren.asari95@gmail.com>
2022-11-13 22:58:26 +0900

    アニメーション追加

--- js/main.js
+++ js/main.js
@@ -7,3 +7,37 @@ $('.hamburger_item a[href]').on('click', function(event) {
     $('.hamburger_menu').trigger('click');
 });
 
+
+
+function fade_up(){
+$('.fadeup_trigger').each(function(){
+    let pos=$(this).offset().top-50;
+    let scroll=$(window).scrollTop();
+    let Wheight=$(window).height();
+
+    if(scroll>=pos-Wheight){
+        $(this).addClass("fadeup");
+    }else{
+        $(this).removeClass('fadeup');
+    }
+
+})
+};
+
+window.onload = function() {
+   $('.fadeup_trigger').removeClass('fadeup');
+  fade_up();
+};
+$(window).scroll(function (){
+    fade_up();
+  });
+
+$('.nav_item a[href]').click(function(){
+    $('.fadeup_trigger').removeClass('fadeup');
+    fade_up()
+});
+
+$('.hamburger_item a[href]').click(function(){
+    $('.fadeup_trigger').removeClass('fadeup');
+    fade_up()
+});
